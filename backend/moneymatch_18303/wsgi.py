97e72df3eb9cc3e991cb1a47bec92b46d124569c
Crowdbotics <team@crowdbotics.com>
2020-06-21 17:39:50 +0000

    Update models

--- backend/moneymatch_18303/wsgi.py
+++ backend/moneymatch_18303/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moneymatch_18303.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moneymatch_18303.settings")
 
 application = get_wsgi_application()
